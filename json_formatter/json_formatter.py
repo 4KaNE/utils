39b47c99333f5c16b342972ed8eedd164c2be549
4KaNE <35267520+4KaNE@users.noreply.github.com>
2018-07-28 11:30:30 +0900

    json_formatter V.2
    ドラッグ&ドロップで起動できるように改良

--- json_formatter/json_formatter.py
+++ json_formatter/json_formatter.py
@@ -1,12 +1,22 @@
 """jsonファイルを整形する"""
+import sys
+import os.path as path
 import json
 
-FILE_NAME = input('>> ')
+def json_format(json_path):
+    file_name = json_path
 
-FILE = open("{}.json".format(FILE_NAME), 'r', encoding="utf-8_sig")
-JSON_DATA = json.load(FILE)
+    json_file = open(path.basename(file_name), 'r', encoding="utf-8_sig")
+    json_data = json.load(json_file)
 
-OUTPUT_JSON = open("{}.json".format(FILE_NAME), "w", encoding="utf-8_sig")
+    output_json = open("{}".format(file_name), "w", encoding="utf-8_sig")
 
-json.dump(JSON_DATA, OUTPUT_JSON, ensure_ascii=False, indent=4, \
-sort_keys=True, separators=(',', ': '))
+    json.dump(json_data, output_json, ensure_ascii=False, indent=4, \
+    sort_keys=True, separators=(',', ': '))
+
+if __name__ == "__main__":
+    print(sys.argv[1])
+    json_format(sys.argv[1])
+    while True:
+        text = input('>> ')
+        print(text)
