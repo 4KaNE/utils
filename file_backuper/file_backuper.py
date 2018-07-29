1b44f1d51623dd5f343621a1d2693e62cf34efbd
4KaNE <35267520+4KaNE@users.noreply.github.com>
2018-07-29 21:00:39 +0900

    正時チェックの追加
    check_on_the_hourを追加
    これでn時ちょうどにループを開始できるぞ！

--- file_backuper/file_backuper.py
+++ file_backuper/file_backuper.py
@@ -1,6 +1,7 @@
 """File backuper"""
 import asyncio
 import datetime
+from time import sleep
 
 def save_file(file_path: str, loop) -> str:
     """Back up the specified file with rename.
@@ -9,10 +10,25 @@ def save_file(file_path: str, loop) -> str:
     """
     print(file_path)
     print(datetime.datetime.now())
-    loop.call_later(10, save_file, file_path, loop)
+    loop.call_later(3600, save_file, file_path, loop)
+
+def check_on_the_hour() -> bool:
+    """Check if the current time is on the hour.
+       return: bool
+    """
+    now = datetime.datetime.now()
+    result = True if now.minute == 0 else False
+    return result
 
 if __name__ == "__main__":
     file_path = "test/test"
+    while True:
+        if check_on_the_hour() is True:
+            break
+        else:
+            print("test")
+            sleep(60)
+
     loop = asyncio.get_event_loop()
     loop.call_soon(save_file, file_path, loop)
     loop.run_forever()
